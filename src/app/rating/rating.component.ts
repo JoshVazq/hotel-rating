4674ab9ed7317db57758c37722b6563db07044ae
Josué Vázquez Rendo <josuevr@ext.inditex.com>
2017-03-08 15:48:29 +0100

    Improve Star Rating Component

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -5,29 +5,35 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
     templateUrl: './rating.component.html',
     styleUrls: ['./rating.component.css']
 })
+//TODO Add @Input ratings
 export class RatingComponent implements OnInit {
-  @Input() itemId: number = 1;
-  inputName: string;
-  ratingValue = 0;
+    @Input() itemId: number = 1;
+    @Input() readonly: false;
+    inputName: string;
+    ratingValue: number = 0;
+    ratings: any[];
 
-  @Output() ratingChange = new EventEmitter();
+    @Output() ratingChange = new EventEmitter();
 
-  @Input()
-  get rating() {
-    return this.ratingValue;
-  }
+    @Input()
+    get rating() {
+        return this.ratingValue;
+    }
 
-  set rating(val) {
-    this.ratingValue = val;
-    this.ratingChange.emit(this.ratingValue);
-  }
+    set rating(val) {
+        this.ratingValue = val;
+        this.ratingChange.emit(this.ratingValue);
+    }
 
-  ngOnInit() {
-    this.inputName = this.itemId + '_rating';
-  }
-
-  onClick(rating: number): void {
-      this.rating = rating;
-  }
+    ngOnInit() {
+        this.inputName = this.itemId + '_rating';
+        this.ratings = [
+            { title: "Rocks!", value: 5 },
+            { title: "Pretty good", value: 4 },
+            { title: "Meh", value: 3 },
+            { title: "Kinda bad", value: 2 },
+            { title: "Sucks big time", value: 1 }
+        ];
+    }
 
 }
